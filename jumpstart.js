6a103001cfdf4c0acb35816fdee508a75831d75d
Robert James Nielsen <robert.j.nielsen@outlook.com>
2020-01-27 14:24:47 -0800

    Server listens.

--- jumpstart.js
+++ jumpstart.js
@@ -3,15 +3,19 @@
 // This will be the main server file (aka: server.js / app.js)
 
 // Declare app dependencies.
+require('dotenv').config();
 const express = require('express');
-const pg = require('pg');
 const superagent = require('superagent');
 const methodOverride = require('method-override');
-require('dotenv').config();
+const pg = require('pg');
 require('ejs');
+
+// Declare lib dependencies.
+const user = require('./lib/user');
 
 // Declare app configs.
 const app = express();
+const PORT = process.env.PORT || 8081;
 const client = new pg.Client(process.env.DATABASE_URL);
 
 // Declare app middleware.
@@ -19,3 +23,12 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 app.use(methodOverride('_method'));
+
+// Declare routes.
+
+// Assign app to connect to database and then listen on PORT.
+client.connect()
+  .then(
+    app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+  )
+  .catch(err => console.error(err))
