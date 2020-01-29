a1029c44fdba1500165e5eff09d8d628c8656129
Jin Woo Kim <jinwoov@gmail.com>
2020-01-29 10:54:48 -0800

    added my page functionality

--- public/js/app.js
+++ public/js/app.js
@@ -36,32 +36,46 @@ function buttonEffect(e) {
     $('#hide-button').hide()
     showSet(0)
     hideSet(6, $('section').length);
+    $('#1').css("font-weight", "bold")
   } else if (click === 1) {
     $('#hide-button').show()
     showSet(6)
     hideSet(0, 6)
     hideSet(12, $('section').length)
+    $('p').css("font-weight", "normal")
+    $('#2').css("font-weight", "bold")
   } else if (click === 2) {
     showSet(12)
     hideSet(0, 12)
     hideSet(18, $('section').length)
+    $('p').css("font-weight", "normal")
+    $('#3').css("font-weight", "bold")
   } else if (click === 3) {
     showSet(18)
     hideSet(0, 18)
     hideSet(24, $('section').length)
+    $('p').css("font-weight", "normal")
+    $('#4').css("font-weight", "bold")
   } else if (click === 4) {
     showSet(24);
     hideSet(0,24);
     hideSet(30, $('section').length);
+    $('p').css("font-weight", "normal")
+    $('#5').css("font-weight", "bold")
   } else if (click === 5) {
     showSet(30);
     hideSet(0,30);
     hideSet(36,$('section').length);
+    $('p').css("font-weight", "normal")
+    $('#6').css("font-weight", "bold")
   } else if (click === 6) {
     showSet(36);
     hideSet(0,36);
+    $('p').css("font-weight", "normal")
+    $('#7').css("font-weight", "bold")
   }
 }
 
+$('#1').css("font-weight", "bold")
 $('#hide-button').hide()
 hideSet(6, $('section').length);
